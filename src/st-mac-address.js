a3267d639425d33abaa8d814c208b10712fdde0e
Marina609 <miss.grigorowi4@ya.ru>
2021-09-30 20:08:26 +0300

    fix: add mac address

--- src/st-mac-address.js
+++ src/st-mac-address.js
@@ -14,7 +14,21 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-export default function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function isMAC48Address(n) {
+  const reg = new RegExp('^([0-9]|[A-F]|-)+$');
+  if (!reg.test(n) || n.length != 17) {
+    return false;
+  }
+
+  const array = n.split('-');
+  if (array.length != 6) {
+    return false;
+  }
+
+  for (let i of array) {
+      if (i.length != 2) {
+        return false;
+      }
+  }
+  return true;
 }
